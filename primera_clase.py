0435033fa58d2a5c581d9b832e06a45c866019f0
BRIGUITTE.SALAS <briguitte.salas@ucr.ac.cr>
2024-12-11 14:17:54 -0600

    Create primera_clase.py
    Primeras declaraciones de codigo en Python y primera practica

---
+++ primera_clase.py
@@ -0,0 +1,26 @@
+#Para imprimir palabras entre comillas
+print ('Hola planeta "tierra"')
+
+#Para imprimir numeros de tipo string
+print("1,2,3")
+
+#Para realizar funciones simples matematicas
+print(120+25)
+
+#input("Dime tu nombre: ")
+
+#Para concatenar se realizar de la siguiente manera: 
+#Primero se corre el input 
+#print("Tu nombre es: " + input("Dime tu nombre: ") + 
+#    input("Dime tu apellido: "))
+
+#Primera Practica------------------------------------------------------
+
+#Crear un codigo para que el usuario pueda responder dos preguntas,
+#que requiera de una sola palabra cada como respuesta
+#luego debe de mostrar en pantalla esas dos palabras combinadas para
+#formar el nombre de una marca de vinos.
+
+print("El nombre para tu marca de Vinos es: " +
+      input("Cual es tu color favorito: ") + " "+
+      input("Describe tu personalidad con una sola palabra: "))
